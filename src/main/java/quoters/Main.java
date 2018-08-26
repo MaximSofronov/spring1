71feb7f8daa10f0fa98c22f00dc1e440d7bc1223
Maksim Sofronov <Maksim_Sofronov@epam.com>
2018-08-26 13:45:05 +0300

    123

--- src/main/java/quoters/Main.java
+++ src/main/java/quoters/Main.java
@@ -5,7 +5,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-        context.getBean(TerminatorQuoter.class).sayQuote();
-
+        while (true) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            context.getBean(Quoter.class).sayQuote();
+        }
     }
 }
