247d1b87c3597284b8b24447244bc5a36029c720
Maksim Sofronov <Maksim_Sofronov@epam.com>
2018-08-26 12:28:47 +0300

    Making annotation

---
+++ src/main/java/quoters/InjectRandomIntAnnottionPostProcessor.java
@@ -0,0 +1,32 @@
+package quoters;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.util.ReflectionUtils;
+import sun.plugin.javascript.ReflectUtil;
+
+import java.lang.reflect.Field;
+import java.util.Random;
+
+public class InjectRandomIntAnnottionPostProcessor implements BeanPostProcessor {
+
+    public Object postProcessBeforeInitialization(Object bean, String s) throws BeansException {
+        final Field[] fields = bean.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            final InjectRandomInt annotation = field.getAnnotation(InjectRandomInt.class);
+            if (annotation != null) {
+                final int min = annotation.min();
+                final int max = annotation.max();
+                Random random = new Random();
+                int i = min + random.nextInt(max - min);
+                field.setAccessible(true);
+                ReflectionUtils.setField(field, bean, i);
+            }
+        }
+        return bean;
+    }
+
+    public Object postProcessAfterInitialization(Object o, String s) throws BeansException {
+        return o;
+    }
+}
